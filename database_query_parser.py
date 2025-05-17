06449b4831a10b6c73663f8d49cb25cf34845dba
ArtemToporkov <toporkovartem259@gmail.com>
2025-05-17 14:12:50 +0500

    db query parser

---
+++ database_query_parser.py
@@ -0,0 +1,7 @@
+class DbQueryParser:
+    def __init__(self, data: dict):
+        self.data = data
+    
+    def parse(data: dict) -> str:
+        # TODO: сделать парснг в запрос для бд
+        return ''
