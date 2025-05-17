ebf4208b1e47658373058827d63646247570b7ab
ArtemToporkov <toporkovartem259@gmail.com>
2025-05-17 14:07:51 +0500

    ai processor class

---
+++ ai_request_processor.py
@@ -0,0 +1,10 @@
+class AiRequestProcessor:
+    def __init__(self, base_url: str, api_key: str = None):
+        self.base_url = base_url
+        self.api_key = api_key
+    
+    def process_query(query: str) -> dict:
+        return {
+
+        }
+
